6df088fb395d8597d6a966717fc2ae6d3a9a8892
tuanorn <tuanminhfor3@gmail.com>
2024-09-28 00:52:43 -0400

    commit 6

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ function App() {
   return <div className='h-[500px] w-full'>
       <Header/>
       <div className="h-3 bg-white"></div>
-      <div className="w-full grid grid-cols-6 gap-3 bg-white">
-        <div className="col-span-5 px-3">
+      <div className="w-full grid grid-cols-6 gap-3 bg-white px-3">
+        <div className="col-span-5">
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/games" element={<Games/>}/>
@@ -19,7 +19,7 @@ function App() {
             <Route path="/projects/PPP" element={<PPP/>}/>
           </Routes>
         </div>
-        <div className="col-span-1 w-full bg-gradient-to-r from-black to-gray-500 flex justify-center items-center w-1/5 aspect-[1/4]">
+        <div className="col-span-1 w-full bg-gradient-to-r from-black to-gray-500 flex justify-center items-center aspect-[1/4]">
             <div className="bg-white" style={{width: "calc(100% - 30px)", height: "calc(100% - 30px)"}}>
                 [ads]
             </div>
